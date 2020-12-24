98299726b3e0221dcd2f0b2cc6e95d9d4651a192
Celine Valentine <cyv419@gmail.com>
2020-12-24 14:35:35 -0800

    colorbox error

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -5,12 +5,14 @@ import colors from './colors';
 const randColor = () => {
 	const randIdx = Math.floor(Math.random() * colors.length);
 	console.log(randIdx);
-	console.log(colors[randIdx]);
+	console.log(colors);
+	console.log(typeof colors[1]);
+	console.log(`${colors[1]}`);
 	return colors[randIdx];
 };
 
 const ColorBox = () => {
-	return <div>className="colorBox" style={{ backgroundColor: randColor }}</div>;
+	return <div className="colorBox" style={{ backgroundColor: randColor }} />;
 };
 
 export { randColor, ColorBox };
